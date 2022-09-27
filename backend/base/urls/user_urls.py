08e3ee332649b7197ac1ac9704449279aae3af1d
unknown <aniketghosh810@gmail.com>
2022-09-27 14:02:36 +0530

    Backend User authentications

---
+++ backend/base/urls/user_urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from base.views import user_views as views
+#Default
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+# )
+
+
+
+urlpatterns = [
+    path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+
+    path('register/', views.registerUser, name='register'),
+    path('profile/', views.getUserProfile, name='users-profile'),
+    path('', views.getUsers, name='users'),
+
+]
--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -1,21 +1,12 @@
-from itertools import product
 from django.shortcuts import render
-from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
-from .models import Product
-# from .products import products
-from .serializers import ProductSerializer
+from base.models import Product
+from base.serializers import ProductSerializer
+from rest_framework import status
 
-
-@api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        'api/products',
-        'api/product/id'
-    ]
-    return Response(routes)
 
 @api_view(['GET'])
 def getProducts(request):
@@ -23,8 +14,9 @@ def getProducts(request):
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
 
+
 @api_view(['GET'])
-def getProduct(request,pk):
+def getProduct(request, pk):
     product = Product.objects.get(_id=pk)
     serializer = ProductSerializer(product, many=False)
     # product = None
